5998aac777f73830e68c4ae21545849a2ee862c2
forever4 <imsuregonnamissu@yahoo.co.uk>
2015-02-22 16:11:11 +0700

    Create Jvs.js

---
+++ Jvs.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+            startGrid(16);
+            $('#container).on('
+                click ',function(){
+                $(this).fadeIn();
+            });
+
+        function startGrid(n) {
+            for (i = 1; i <= n; i++) {
+                $('#container').append('<div class="basic"></div>');
+            };
